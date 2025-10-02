b0a5137431a1513621e37a0d189fa67e11540e84
Henry Monteith <hi@henry.moe>
2025-10-02 13:52:48 -0400

    add json.simple to module-info.java

--- exitprotocol/src/main/java/module-info.java
+++ exitprotocol/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.theclankers {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens com.theclankers to javafx.fxml;
     exports com.theclankers;
