979e5d0fd13dc5ef7b4b5175312e252ef0986afd
breadwards88 <breadwards88@gmail.com>
2025-10-08 02:13:25 -0400

    Added #45 createGame method, Started on #44 getGames in GamesList, added #61 Progress method, added #62 getProgress class stub, Added Challenge class and added class stubs for it

--- exitprotocol/src/main/java/com/model/GameList.java
+++ exitprotocol/src/main/java/com/model/GameList.java
@@ -1,9 +1,14 @@
 package com.model;
 
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.List;
+
 public class GameList {
     private static GameList gameList;
 
     private GameList() {}
+    private ArrayList<Game> games = new ArrayList<>();
 
     public static GameList getInstance() {
         if (gameList == null) {
@@ -13,7 +18,30 @@ public class GameList {
         return gameList;
     }
 
-    public void createGame(String theme, int difficulty, int playerCout, String teamName) {}
+    public void createGame(String theme, int difficulty, int playerCount, String teamName) {
+        games.add(new Game(theme, difficulty, playerCount, teamName));
+    }
+
     // public Game getGame() {}
+
+    public void getGames(UUID id)
+    {
+        ArrayList<Game> gamesbyid = new ArrayList<>();
+        /* 
+        for(Game game : gameList)
+        {
+            
+            if (game.getUUID().equals(id))
+            {
+
+            }
+            
+            //cannot finish method until GamesList is made
+        }
+            */
+    }
+
+    
+
     public void saveGame() {}
 }
