b1582147aeca396f629901219e2e56b847ddfe59
breadwards88 <breadwards88@gmail.com>
2025-10-15 09:25:18 -0400

    Added Methods to Challenge, removed Methods from InteractiveChallenge

--- exitprotocol/src/main/java/com/model/Challenge.java
+++ exitprotocol/src/main/java/com/model/Challenge.java
@@ -3,8 +3,18 @@ package com.model;
 import java.util.HashMap;
 import java.util.UUID;
 import java.util.ArrayList;
+import javafx.scene.image.Image;
 public class Challenge {
     HashMap<String, Integer> scoreSet = new HashMap<>();
+    private String answer;
+    private String question;
+    private String hint;
+    private Image clue;
+    private ArrayList<String> hints;
+
+    public Challenge(String answer, String question, ArrayList<String> hints, Image clue){
+
+    }
 
     public void addScore(UUID id, Integer score)
     {
@@ -32,21 +42,31 @@ public class Challenge {
 
     public String getAnswer()
     {
-        return null;
+        return this.answer;
     }
 
-    public void setAnswer(String answer)
+    public void setAnswer(String theAnswer)
     {
-
+        this.answer = theAnswer;
     }
 
     public ArrayList<String> getHints()
     {
-        return null;
+        return this.hints;
     }
 
     public void addHint(String hint)
     {
+        hints.add(hint);
+    }
 
+    public void setClue(Image theClue)
+    {
+        this.clue = theClue;
+    }
+
+    public Image getClue()
+    {
+        return clue;
     }
 }
--- exitprotocol/src/main/java/com/model/InteractiveChallenge.java
+++ exitprotocol/src/main/java/com/model/InteractiveChallenge.java
@@ -2,12 +2,9 @@ package com.model;
 
 import java.util.ArrayList;
 import javafx.scene.image.Image;
-
-public class InteractiveChallenge {
-    private String answer;
-    private ArrayList<String> hints;
-    private Image clue;
-    /* 
+/* 
+public class InteractiveChallenge extends Challenge{
+    
     public String getQuestion()
     {
         return null;
@@ -17,34 +14,11 @@ public class InteractiveChallenge {
     {
         this.question = newQuestion;
     }
-*/
-    public String getAnswer()
-    {
-        return this.answer;
-    }
 
-    public void setAnswer(String theanswer)
-    {
-        this.answer = theanswer;
-    }
 
-    public ArrayList<String> getHints()
-    {
-        return this.hints;
-    }
 
-    public void addHint(String hint)
-    {
-        hints.add(hint);
-    }
+    
 
-    public void setClue(Image theClue)
-    {
-        this.clue = theClue;
-    }
-
-    public Image getClue()
-    {
-        return clue;
-    }
+    
 }
+*/
