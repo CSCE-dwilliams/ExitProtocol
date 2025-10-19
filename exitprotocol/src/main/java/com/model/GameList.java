9314d0f1071cc487269d509d1101ae4024f574b9
breadwards88 <breadwards88@gmail.com>
2025-10-19 19:08:21 -0400

    Fixed GameList and createGame method

--- exitprotocol/src/main/java/com/model/GameList.java
+++ exitprotocol/src/main/java/com/model/GameList.java
@@ -1,19 +1,20 @@
 package com.model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.UUID;
 import java.util.List;
 
 public class GameList {
     private static GameList gameList;
-    private HashMap<UUID, Game> games = new HashMap<>();
-    
+
     private GameList() {}
+    private ArrayList<Game> games = new ArrayList<>();
+
     public static GameList getInstance() {
         if (gameList == null) {
             gameList = new GameList();
         }
+
         return gameList;
     }
 
@@ -23,35 +24,7 @@ public class GameList {
 
     // public Game getGame() {}
 
-    public void getGames(UUID id)
-    {
-        ArrayList<Game> gamesbyid = new ArrayList<>();
-        /* 
-        for(Game game : gameList)
-        {
-            
-            if (game.getUUID().equals(id))
-            {
-
-            }
-            
-            //cannot finish method until GamesList is made
-        }
-    }
-
-    public void addgame(Game g){
-        games.put(g.getGameID(),g);
-    }
     
-    public Game getGame(UUID id){
-        return games.get(id);
-    }  
-
-    // public void createGame(String theme, int difficulty, int playerCount, String teamName) {
-    //     games.add(new Game(theme, difficulty, playerCount, teamName, null));
-
-
-    // }
 
     public void saveGame() {}
 }
