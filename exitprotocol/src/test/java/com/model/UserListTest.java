4cbcd8b9b0c674992020d3cde4c44bc56f1eaac5
breadwards88 <breadwards88@gmail.com>
2025-11-02 17:09:05 -0500

    Deleted un-needed Test Classes

--- exitprotocol/src/test/java/com/model/UserListTest.java
+++ exitprotocol/src/test/java/com/model/UserListTest.java
@@ -1,5 +1,26 @@
 package com.model;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.Assert;
+import com.model.*;
+
 public class UserListTest {
-    
+    @Test
+    public void testTesting(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void testValidLogin(){
+        Library library = Library.getInstance();
+        library.login();
+        String email = library.getCurrentUser().getEmail().toLowerCase();
+    }
+
+    @Test
+    public void test(){
+
+    }
 }
