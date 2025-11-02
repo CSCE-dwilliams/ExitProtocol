08c870ad5260615378256c865e8421f9e4793513
breadwards88 <breadwards88@gmail.com>
2025-11-02 17:10:51 -0500

    Removed my own changes from #106 since its already been done

--- exitprotocol/src/test/java/com/model/UserListTest.java
+++ exitprotocol/src/test/java/com/model/UserListTest.java
@@ -7,20 +7,5 @@ import org.junit.Assert;
 import com.model.*;
 
 public class UserListTest {
-    @Test
-    public void testTesting(){
-        assertTrue(true);
-    }
-
-    @Test
-    public void testValidLogin(){
-        Library library = Library.getInstance();
-        library.login();
-        String email = library.getCurrentUser().getEmail().toLowerCase();
-    }
-
-    @Test
-    public void test(){
-
-    }
+    
 }
