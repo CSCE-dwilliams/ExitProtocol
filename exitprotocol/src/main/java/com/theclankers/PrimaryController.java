88b5475049e4a0a7c12c8c2937e89516ce1068b0
Henry Monteith <hi@henry.moe>
2025-11-11 14:25:01 -0500

    add javafx files

--- exitprotocol/src/main/java/com/theclankers/PrimaryController.java
+++ exitprotocol/src/main/java/com/theclankers/PrimaryController.java
@@ -1,6 +1,7 @@
 package com.theclankers;
 
 import java.io.IOException;
+
 import javafx.fxml.FXML;
 
 public class PrimaryController {
@@ -8,5 +9,15 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML
+    private void switchToSignInScreen() throws IOException {
+        System.exit(0);
+    }
+
+    @FXML
+    private void switchToCreateAccountScreen() throws IOException {
+        System.exit(0);
     }
 }
