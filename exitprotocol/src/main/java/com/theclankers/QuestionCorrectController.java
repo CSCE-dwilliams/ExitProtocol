8932cb0c69cea192ef3e1f8a526c6a3ea233fc04
gonzalespaz-e <emmanuel.gp.82@gmail.com>
2025-12-05 16:42:46 -0500

    added qcorrect controller

---
+++ exitprotocol/src/main/java/com/theclankers/QuestionCorrectController.java
@@ -0,0 +1,14 @@
+package com.theclankers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class QuestionCorrectController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+}
