344ee31054cc9b8b04baff32d4cb2f87340d2f33
gonzalespaz-e <emmanuel.gp.82@gmail.com>
2025-12-05 16:56:15 -0500

    minor controller change

--- exitprotocol/src/main/java/com/theclankers/QuestionCorrectController.java
+++ exitprotocol/src/main/java/com/theclankers/QuestionCorrectController.java
@@ -3,9 +3,13 @@ package com.theclankers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 public class QuestionCorrectController implements Initializable {
+    @FXML
+    Button continueBtn;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
